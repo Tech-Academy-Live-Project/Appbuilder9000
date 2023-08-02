a2f25d10d88158a76ed763dccb764dbb99d29310
jckyagno <133424867+jckyagno@users.noreply.github.com>
2023-08-02 13:10:56 -1000

    Completed Story 2
    Added in the ability to enroll profiles. Modified some CSS to look more visually appealing.

--- Appbuilder9000/JA3Mercs/views.py
+++ Appbuilder9000/JA3Mercs/views.py
@@ -1,12 +1,59 @@
 from django.shortcuts import render, redirect,  get_object_or_404
+from .models import Merc
+from .forms import MercForm
 
 
-
-
-
-# Story #1: Build the basic app ----------------------------------------------------------------------------------------
 
 def ja3_home(request):
     return render(request, 'JA3Mercs/ja3_home.html')
 
-# Create your views here.
+
+def ja3_enroll(request):
+    mercs = Merc.objects.all()
+    return render(request, 'JA3Mercs/ja3_enroll.html', {'mercs': mercs})
+
+
+def details(request, pk):
+    pk = int(pk)
+    item = get_object_or_404(Merc, pk=pk)
+    form = MercForm(data=request.POST or None, instance=item)
+    if request.method == 'POST':
+        if form.is_valid():
+            form2 = form.save(commit=False)
+            form2.save()
+            return redirect('ja3_enroll')
+        else:
+            print(form.errors)
+    else:
+        return render(request, 'JA3Mercs/ja3_details.html', {'form': form})
+
+
+
+def delete(request, pk):
+    pk = int(pk)
+    item = get_object_or_404(Merc, pk=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('ja3_enroll')
+    context = {"item": item,}
+    return render(request, "JA3Mercs/ja3_confirmDelete.html", context)
+
+def confirmed(request):
+    if request.method == 'POST':
+        form = MercForm(request.POST or None)
+        if form.is_valid():
+            form.delete()
+            return redirect('ja3_enroll')
+    else:
+        return redirect('ja3_enroll')
+
+def createRecord(request):
+    form = MercForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('ja3_enroll')
+    else:
+        print(form.errors)
+        form = MercForm()
+    context = { 'form': form }
+    return render(request, "JA3Mercs/ja3_createRecord.html", context)
